fd5deb6453044cec3789232a428703d70a7643a3
Michael Sky <electrydev@gmail.com>
2017-05-06 22:50:09 +0200

    grouper: Update blobs with extract_files.sh
    * Merge blobs from following repos:
    * https://github.com/Unlegacy-Android/
    proprietary_vendor_asus fec1905
    proprietary_vendor_broadcom c3387c6
    proprietary_vendor_invensense 38a68ef
    proprietary_vendor_nvidia 0ad470d
    proprietary_vendor_widevine 8877055
    
    Change-Id: Ia02ee680956a7023368db6ead25af04d40e50580
    Signed-off-by: Michael Sky <electrydev@gmail.com>

--- grouper/asus-vendor-blobs.mk
+++
@@ -1,7 +0,0 @@
-VENDOR_FOLDER := vendor/asus/grouper
-
-PRODUCT_COPY_FILES += \
-	$(VENDOR_FOLDER)/proprietary/etc/nvcamera.conf:system/etc/nvcamera.conf \
-	$(VENDOR_FOLDER)/proprietary/etc/nvram.txt:system/etc/nvram.txt \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/hw/sensors.grouper.so:system/vendor/lib/hw/sensors.grouper.so \
-	$(VENDOR_FOLDER)/proprietary/vendor/lib/libsensors.lightsensor.so:system/vendor/lib/libsensors.lightsensor.so
